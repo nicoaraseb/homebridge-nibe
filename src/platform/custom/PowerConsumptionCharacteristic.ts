df8bb39ef136fee3b59b3f90b1ef26bc47e708ee
Sebastian Nicoara <sebastian.nicoara@kemiex.com>
2024-12-30 17:30:37 +0200

    try

--- src/platform/custom/PowerConsumptionCharacteristic.ts
+++ src/platform/custom/PowerConsumptionCharacteristic.ts
@@ -1,18 +1,22 @@
 import { Characteristic, CharacteristicProps, Service } from 'hap-nodejs';
 
-// Create a custom characteristic for temperature (or any other data)
+// Create a custom characteristic for Power Consumption (or any other data)
 class PowerConsumptionCharacteristic extends Characteristic {
   constructor() {
-    super('PowerConsumptionCharacteristic', '1dc7fa25-0a30-4796-9f2e-a897a80a95bd',
-    {
-      format: Characteristic.Formats.FLOAT,
-      unit: 'kWh', // You can also use 'Fahrenheit' or other units
-      minValue: -100, // Example minimum value
-      maxValue: 100000, // Example max value
-      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY],
-    });
+    super(
+      'PowerConsumptionCharacteristic',
+      '1dc7fa25-0a30-4796-9f2e-a897a80a95bd', // Unique UUID
+      {
+        format: Characteristic.Formats.FLOAT,   // Data format (FLOAT)
+        unit: 'kWh',                           // Unit of measure (kWh)
+        minValue: -100,                        // Example minimum value
+        maxValue: 100000,                      // Example maximum value
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY], // Permissions
+      }
+    );
     this.value = 0; // Default value
   }
-
-  export { PowerConsumptionCharacteristic };
 }
+
+// Export the custom characteristic so it can be used elsewhere
+export { PowerConsumptionCharacteristic };
