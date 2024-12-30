a08978879374696344df056c2507c8aee361f9bd
Sebastian Nicoara <sebastian.nicoara@kemiex.com>
2024-12-30 15:53:24 +0200

    fix

--- src/platform/customcharacteristics/TotalConsumptionCharacteristic.ts
+++ src/platform/customcharacteristics/TotalConsumptionCharacteristic.ts
@@ -3,13 +3,13 @@ import { Characteristic, CharacteristicProps, UUID } from 'hap-nodejs';
 // Define a custom TransmitPower characteristic
 export class TotalConsumptionCharacteristic extends Characteristic {
   constructor() {
-    super('TotalConsumptionCharacteristic', UUID.generate('TotalConsumptionCharacteristic'), // Unique UUID
+    super('TotalConsumptionCharacteristic', '48128b2d-0bf7-41f8-b30b-53f3a7ccf49b', // Unique UUID
     this.setProps({
       format: Characteristic.Formats.FLOAT,
       unit: 'kWh',
       minValue: 0,
       maxValue: 2000000,
-      stepValue: 0.1,
+      stepValue: 1,
       perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
     }));
     this.value = this.getDefaultValue();
