00d60f11ce4307cffc31f182ae67d7e153fcb3da
Sebastian Nicoara <sebastian.nicoara@kemiex.com>
2024-12-30 17:24:37 +0200

    try

--- src/platform/custom/PowerConsumptionCharacteristic.ts
+++ src/platform/custom/PowerConsumptionCharacteristic.ts
@@ -3,8 +3,8 @@ import { Characteristic, CharacteristicProps, Service } from 'hap-nodejs';
 // Create a custom characteristic for temperature (or any other data)
 class PowerConsumptionCharacteristic extends Characteristic {
   constructor() {
-    super('PowerConsumptionCharacteristic', '1dc7fa25-0a30-4796-9f2e-a897a80a95bd');
-    this.setProps({
+    super('PowerConsumptionCharacteristic', '1dc7fa25-0a30-4796-9f2e-a897a80a95bd',
+    {
       format: Characteristic.Formats.FLOAT,
       unit: 'kWh', // You can also use 'Fahrenheit' or other units
       minValue: -100, // Example minimum value
